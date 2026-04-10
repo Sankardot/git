b2cfe09194a79c0c5d68419d9bfb1c7d915d1b49
sankar <sankarnarayana910@gmail.com>
2026-04-10 16:20:54 +0530

    up

---
+++ hari.py
@@ -0,0 +1,107 @@
+# control statements : is used change the flow of execution 
+# it is divided into 3 types 
+# they are 
+
+
+# conditonal staments , jumping staments , looping staments 
+
+
+# conditioanl staments is aslo called decison maiking staments 
+# if , if else, elif else
+
+
+
+# syntax:
+
+# if condition:
+#     statement
+# elif condition:
+#     statement
+# else:
+#     statement
+
+
+# num1 = int(input("enter first number: "))
+# num2 = int(input("enter ysecond number: "))
+
+# if num1 > num2:
+#     print("num1 is greater than num2")
+# elif num2 > num1:
+#     print("num2 is greate rthan num1")
+# else:
+#     print("thank you")
+
+
+# marks = 7
+# if marks >=90:
+#     print("A")
+# elif marks >=70:
+#     print("B")
+
+# else:
+#     print("Fial")
+
+
+# q=input("enter the text:")
+# print("hello",q)
+
+# looping staments:
+#     it is calleed iterative staments 
+    
+#     for lopp and while loop 
+    # if we want excute a bloclk code are multiple times 
+    # then we should use loopinf kind of execution 
+    
+    # for loop , while loop
+    
+    
+# for  i in "kiran":
+#     print(i)
+    
+    
+    
+# for i in range (0, 9):
+#     print(i)
+
+
+# while loop : repeat or group of staments while 
+# given condition is true it tests the condtion before executing the loop body
+
+
+# i = 1
+# while i<100:
+#     print(i)
+#     i+=1
+
+
+# password = "hello123"
+# user_input =""
+# while user_input != password:
+#     user_input=input("enter the password: ")
+# print("unlocked thank you visit agian")
+
+
+
+# glasses = 4
+# while glasses < 8:
+#     print("drink water")
+#     glasses +=1
+# print("daily glas completed")
+
+
+
+# jumping staments : jumping stamentsare are also called  tranfer staments used to control the flow  
+# of a program by jumping from one part of code to antother part of code 
+
+
+# break continue pass 
+
+
+for i in range (0,10, 2):
+    if i == 3:
+        break
+    print(i)
+    
+    
+#range function  (start, stop, step)
+   
